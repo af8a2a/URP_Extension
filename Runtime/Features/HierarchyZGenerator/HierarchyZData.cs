dd8c5e34eb82f19d30a9b9018b32c1ba2db3057a
af8a2a <1125209282@qq.com>
2025-04-12 20:46:42 +0800

    init

---
+++ Runtime/Features/HierarchyZGenerator/HierarchyZData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+
+namespace URP_Extension.Features.HierarchyZGenerator
+{
+    public class HierarchyZData : ContextItem
+    {
+        public TextureHandle HizTexture = TextureHandle.nullHandle;
+        public int MipCount = 0;
+        public override void Reset()
+        {
+            HizTexture = TextureHandle.nullHandle;
+        }
+    }
+}
