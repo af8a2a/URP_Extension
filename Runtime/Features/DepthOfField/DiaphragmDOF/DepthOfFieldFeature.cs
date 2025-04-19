341a5d13c6975d82eea4b64b9abb2b973d9e556f
af8a2a <1125209282@qq.com>
2025-04-19 10:07:56 +0800

    feat:HDRP Dof

---
+++ Runtime/Features/DepthOfField/DiaphragmDOF/DepthOfFieldFeature.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Features.DepthOfField.DiaphragmDOF
+{
+    public class DepthOfFieldFeature : ScriptableRendererFeature
+    {
+        DiaphragmDoFPass diaphragmDoFPass;
+
+        public override void Create()
+        {
+            diaphragmDoFPass = new DiaphragmDoFPass();
+            diaphragmDoFPass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        }
+
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            var setting = VolumeManager.instance.stack.GetComponent<PhysicallyDepthOfField>();
+            if (setting == null || !setting.IsActive())
+            {
+                return;
+            }
+
+            diaphragmDoFPass.Setup(setting);
+
+            renderer.EnqueuePass(diaphragmDoFPass);
+        }
+    }
+}
