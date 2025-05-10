6a56b0c72010497cdc57824cc7637785d7b637d6
af8a2a <1125209282@qq.com>
2025-05-10 18:32:37 +0800

    feat:Mobile Bloom

---
+++ Runtime/Features/Postprocessing/Bloom/MobileBloom/MobileBloomFeature.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Rendering.Universal;
+
+namespace Features.Postprocessing.Bloom.MobileBloom
+{
+    public class MobileBloomFeature:ScriptableRendererFeature
+    {
+        private MobileBloomPass pass;
+        public override void Create()
+        {
+            pass = new MobileBloomPass()
+            {
+                renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing
+            };
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(pass);
+        }
+    }
+}
