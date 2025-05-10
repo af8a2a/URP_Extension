7d8d8f33088184db2a33bd613928240e729b0e57
af8a2a <1125209282@qq.com>
2025-05-10 09:45:58 +0800

    feat:GranTurismo ToneMapping

---
+++ Runtime/Features/Postprocessing/ToneMapping/GranTurismoToneMappingFeature.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Rendering.Universal;
+
+namespace Features.Postprocessing.ToneMapping
+{
+    public class GranTurismoToneMappingFeature : ScriptableRendererFeature
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+
+        private GranTurismoToneMappingPass _pass;
+
+        public override void Create()
+        {
+            _pass = new GranTurismoToneMappingPass()
+            {
+                renderPassEvent = renderPassEvent
+            };
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(_pass);
+        }
+    }
+}
