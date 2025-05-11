12a8450ddcc5f7fc5ad421a041c3e1f2f990595a
af8a2a <1125209282@qq.com>
2025-05-11 16:49:47 +0800

    feat:PerObjectShadow from https://github.com/danbaidong1111/DanbaidongRP

---
+++ Runtime/Features/Shadow/Shadows.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Features.Shadow
+{
+    public sealed partial class Shadows : VolumeComponent, IPostProcessComponent
+    {
+        [Tooltip("Penumbra controls shadows soften width. (For Per Object Shadow)")]
+        public ClampedFloatParameter perObjectShadowPenumbra = new ClampedFloatParameter(1.0f, 0.001f, 3.0f);
+
+
+        /// <inheritdoc/>
+        public bool IsActive() => true; // Always enable screenSpaceShadows.
+    }
+}
