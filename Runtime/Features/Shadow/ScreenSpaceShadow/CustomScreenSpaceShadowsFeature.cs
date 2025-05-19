d210eae6a0a0f73252c4a8187c2549d3fe389d2f
af8a2a <1125209282@qq.com>
2025-05-20 00:09:02 +0800

    feat:CascadeShadow editor extend

---
+++ Runtime/Features/Shadow/ScreenSpaceShadow/CustomScreenSpaceShadowsFeature.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Features.Shadow.ScreenSpaceShadow.URPShadow;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+
+namespace Features.Shadow.ScreenSpaceShadow
+{
+    //prepare for global setting
+    [Serializable]
+    internal class ScreenSpaceShadowsSettings
+    {
+    }
+
+    [SupportedOnRenderer(typeof(UniversalRendererData))]
+    [DisallowMultipleRendererFeature("Custom Screen Space Shadows")]
+    public class CustomScreenSpaceShadowsFeature : ScriptableRendererFeature
+    {
+#if UNITY_EDITOR
+        [UnityEditor.ShaderKeywordFilter.SelectIf(true, keywordNames: ShaderKeywordStrings.MainLightShadowScreen)]
+        private const bool k_RequiresScreenSpaceShadowsKeyword = true;
+#endif
+
+        [SerializeField] private ScreenSpaceShadowsSettings m_Settings = new ScreenSpaceShadowsSettings();
+
+        private URPScreenSpaceShadowsPass m_SSShadowsPass = null;
+        private ScreenSpaceShadowsPostPass m_SSShadowsPostPass = null;
+
+
+        /// <inheritdoc/>
+        public override void Create()
+        {
+            if (m_SSShadowsPass == null)
+                m_SSShadowsPass = new URPScreenSpaceShadowsPass();
+            if (m_SSShadowsPostPass == null)
+                m_SSShadowsPostPass = new ScreenSpaceShadowsPostPass();
+
+            m_SSShadowsPass.renderPassEvent = RenderPassEvent.AfterRenderingGbuffer;
+            m_SSShadowsPostPass.renderPassEvent = RenderPassEvent.BeforeRenderingTransparents;
+        }
+
+
+        /// <inheritdoc/>
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (UniversalRenderer.IsOffscreenDepthTexture(ref renderingData.cameraData))
+                return;
+
+
+            var shadowSettings = VolumeManager.instance.stack.GetComponent<Shadows>();
+
+
+            var algo = ShadowAlgo.URP;
+            bool overrideSetting = !(shadowSettings is null || !shadowSettings.IsActive());
+
+            if (overrideSetting)
+            {
+                algo = shadowSettings.shadowAlgo.value;
+            }
+
+            bool usesDeferredLighting = renderer is UniversalRenderer { usesDeferredLighting: true };
+
+
+            switch (algo)
+            {
+                case ShadowAlgo.URP:
+                    bool allowMainLightShadows = renderingData.shadowData.supportsMainLightShadows && renderingData.lightData.mainLightIndex != -1;
+
+                    bool shouldEnqueue = allowMainLightShadows && m_SSShadowsPass.Setup(m_Settings);
+
+                    if (shouldEnqueue)
+                    {
+                        m_SSShadowsPass.renderPassEvent = usesDeferredLighting
+                            ? RenderPassEvent.AfterRenderingGbuffer
+                            : RenderPassEvent.AfterRenderingPrePasses +
+                              1; // We add 1 to ensure this happens after depth priming depth copy pass that might be scheduled
+
+                        renderer.EnqueuePass(m_SSShadowsPass);
+                        renderer.EnqueuePass(m_SSShadowsPostPass);
+                    }
+
+                    break;
+                case ShadowAlgo.CSM8:
+
+                    renderer.EnqueuePass(m_SSShadowsPostPass);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+
+        private class ScreenSpaceShadowsPostPass : ScriptableRenderPass
+        {
+            internal ScreenSpaceShadowsPostPass()
+            {
+                profilingSampler = new ProfilingSampler("Set Screen Space Shadow Keywords");
+            }
+
+
+            private static void ExecutePass(RasterCommandBuffer cmd, UniversalShadowData shadowData)
+            {
+                int cascadesCount = shadowData.mainLightShadowCascadesCount;
+                bool mainLightShadows = shadowData.supportsMainLightShadows;
+                bool receiveShadowsNoCascade = mainLightShadows && cascadesCount == 1;
+                bool receiveShadowsCascades = mainLightShadows && cascadesCount > 1;
+
+                // Before transparent object pass, force to disable screen space shadow of main light
+                cmd.SetKeyword(ShaderGlobalKeywords.MainLightShadowScreen, false);
+
+                // then enable main light shadows with or without cascades
+                cmd.SetKeyword(ShaderGlobalKeywords.MainLightShadows, receiveShadowsNoCascade);
+                cmd.SetKeyword(ShaderGlobalKeywords.MainLightShadowCascades, receiveShadowsCascades);
+            }
+
+
+            internal class PassData
+            {
+                internal ScreenSpaceShadowsPostPass pass;
+                internal UniversalShadowData shadowData;
+            }
+
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                using (var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData, profilingSampler))
+                {
+                    UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+
+                    TextureHandle color = resourceData.activeColorTexture;
+                    builder.SetRenderAttachment(color, 0, AccessFlags.Write);
+                    passData.shadowData = frameData.Get<UniversalShadowData>();
+                    passData.pass = this;
+
+                    builder.AllowGlobalStateModification(true);
+
+                    builder.SetRenderFunc((PassData data, RasterGraphContext rgContext) => { ExecutePass(rgContext.cmd, data.shadowData); });
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/Features/Shadow/ShadowCommon/ShadowUtilsExt.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Features.Shadow
+{
+    public static class ShadowUtilsExt
+    {
+        internal static void RenderShadowSliceNoOffset(RasterCommandBuffer cmd,
+            ref ShadowSliceData shadowSliceData, ref RendererList shadowRendererList,
+            Matrix4x4 proj, Matrix4x4 view)
+        {
+            cmd.SetGlobalDepthBias(1.0f,
+                2.5f); // these values match HDRP defaults (see https://github.com/Unity-Technologies/Graphics/blob/9544b8ed2f98c62803d285096c91b44e9d8cbc47/com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/HDShadowAtlas.cs#L197 )
+
+            cmd.SetViewProjectionMatrices(view, proj);
+            if (shadowRendererList.isValid)
+                cmd.DrawRendererList(shadowRendererList);
+
+            cmd.DisableScissorRect();
+            cmd.SetGlobalDepthBias(0.0f, 0.0f); // Restore previous depth bias values
+        }
+
+
+        static Vector3 s_camCenter = Vector3.zero;
+        static Vector3 s_TR_Dir = Vector3.zero;
+
+        public static bool ComputeDirectionalShadowMatricesAndCullingSphere
+        (ref UniversalCameraData cameraData, ref UniversalShadowData shadowData, int cascadeIndex, Light light, int shadowResolution
+            , out Vector4 cullingSphere, out Matrix4x4 viewMatrix, out Matrix4x4 projMatrix, out float ZDistance)
+        {
+            var camNear = 0f;
+            var s_BL_Dir = Vector3.zero;
+            if (cascadeIndex == 0)
+            {
+                camNear = cameraData.camera.nearClipPlane;
+                Matrix4x4 VP = cameraData.GetProjectionMatrix() * cameraData.GetViewMatrix();
+                Matrix4x4 I_VP = VP.inverse;
+
+                Vector3 nearBL = I_VP.MultiplyPoint(new Vector3(-1, -1, -1));
+                Vector3 nearTR = I_VP.MultiplyPoint(new Vector3(1, 1, -1));
+
+                s_camCenter = cameraData.camera.transform.position;
+                s_BL_Dir = nearBL - s_camCenter;
+                s_TR_Dir = nearTR - s_camCenter;
+            }
+
+
+
+            float cascadeFar = camNear + cameraData.maxShadowDistance * shadowData.mainLightShadowCascadesSplitArray[cascadeIndex];
+            float cascadeNear = camNear;
+            if (cascadeIndex > 0)
+            {
+                cascadeNear = camNear + cameraData.maxShadowDistance * shadowData.mainLightShadowCascadesSplitArray[cascadeIndex - 1];
+            }
+
+            Vector3 cascadeNearBL = s_camCenter + s_BL_Dir / camNear * cascadeNear;
+            Vector3 cascadeNearTR = s_camCenter + s_TR_Dir / camNear * cascadeNear;
+
+            Vector3 cascadeFarBL = s_camCenter + s_BL_Dir / camNear * cascadeFar;
+            Vector3 cascadeFarTR = s_camCenter + s_TR_Dir / camNear * cascadeFar;
+
+            //sphere bounding box
+            float a = Vector3.Distance(cascadeNearBL, cascadeNearTR);
+            float b = Vector3.Distance(cascadeFarBL, cascadeFarTR);
+            float l = cascadeFar - cascadeNear;
+
+            float x = (b * b - a * a) / (8 * l) + l / 2;
+
+            Vector3 sphereCenter = cameraData.camera.transform.position + cameraData.camera.transform.forward * (cascadeNear + x);
+            float sphereR = Mathf.Sqrt(x * x + a * a / 4);
+
+            //Anti-Shimmering
+            if (cascadeIndex < 4)
+            {
+                float squrePixelWidth = 2 * sphereR / shadowResolution;
+                Vector3 sphereCenterLS = light.transform.worldToLocalMatrix.MultiplyPoint(sphereCenter);
+                sphereCenterLS.x /= squrePixelWidth;
+                sphereCenterLS.x = Mathf.Floor(sphereCenterLS.x);
+                sphereCenterLS.x *= squrePixelWidth;
+                sphereCenterLS.y /= squrePixelWidth;
+                sphereCenterLS.y = Mathf.Floor(sphereCenterLS.y);
+                sphereCenterLS.y *= squrePixelWidth;
+                sphereCenter = light.transform.localToWorldMatrix.MultiplyPoint(sphereCenterLS);
+            }
+
+
+            cullingSphere.x = sphereCenter.x;
+            cullingSphere.y = sphereCenter.y;
+            cullingSphere.z = sphereCenter.z;
+            cullingSphere.w = sphereR;
+
+            float backDistance = sphereR * light.shadowNearPlane * 10;
+            Vector3 shadowMapEye = sphereCenter - light.transform.forward * backDistance;
+            Vector3 shadowMapAt = sphereCenter;
+
+            Matrix4x4 lookMatrix = Matrix4x4.LookAt(shadowMapEye, shadowMapAt, light.transform.up);
+            // Matrix that mirrors along Z axis, to match the camera space convention.
+            Matrix4x4 scaleMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, -1));
+            // Final view matrix is inverse of the LookAt matrix, and then mirrored along Z.
+            viewMatrix = scaleMatrix * lookMatrix.inverse;
+
+            projMatrix = Matrix4x4.Ortho(-sphereR, sphereR, -sphereR, sphereR, 0.0f, 2.0f * backDistance);
+            ZDistance = 2.0f * backDistance;
+
+            return true;
+        }
+    }
+}
